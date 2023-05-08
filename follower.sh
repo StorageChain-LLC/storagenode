853a3aee42a34c1fcbf78301bc0b9d7e1a42ff64
Sheraz Ahmed <sheraz@invozone.com>
2023-05-08 14:24:06 +0500

    Updated Cluster Config

--- follower.sh
+++ follower.sh
@@ -1,4 +1,4 @@
 # echo "hosts: files dns" > /etc/nsswitch.conf
-ipfs-cluster-follow cluster_follower init http://46.101.179.114:8080/ipfs/QmNj7guXKAo67nZwE6XtyfhgDnXsdpa8Xte3MVH7TjaUoM
+ipfs-cluster-follow cluster_follower init http://46.101.179.114:8080/ipfs/QmTUxPJSSmkFzVHZj9GvWzaKSmAZ5i3Eye5sWY77vqMkJv
 ipfs-cluster-ctl --host /unix//root/.ipfs-cluster-follow/cluster_follower/api-socket
 ipfs-cluster-follow cluster_follower run
