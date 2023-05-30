c57a98aae8108bcbcf5dcba74df521f64b0631be
Sheraz Ahmed <sheraz@invozone.com>
2023-05-30 17:26:48 +0500

    Updated configuration to allow cluster configuration

--- follower.sh
+++ follower.sh
@@ -1,3 +1,3 @@
-ipfs-cluster-follow cluster_follower init http://46.101.179.114:8080/ipfs/QmR6tsjp1HgpbyipPH3MQfhgjq6d2LFqi3Vgj9W2NWQqsW
+ipfs-cluster-follow cluster_follower init http://46.101.179.114:8080/ipfs/QmRNJD4dXVfaqSfskY8LT2mdxbELiHVAkqPuWdz9WgQfxS
 ipfs-cluster-follow cluster_follower run
 # curl -X POST "http://103.73.101.34:5001/api/v0/bootstrap/add?arg=/ip4/46.101.179.114/tcp/4001/p2p/12D3KooWQcT1db4e1XiWc2HqvCwrMeTANr3QeQquynJ3WHTB4ucV"
