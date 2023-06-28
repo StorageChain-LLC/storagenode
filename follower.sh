d82fe3b0759f729e4d13c399010fd13c81231d97
Sheraz Ahmed <sheraz@invozone.com>
2023-06-28 20:09:03 +0500

    Updated Docker Configurations

--- follower.sh
+++ follower.sh
@@ -1,3 +1,2 @@
 ipfs-cluster-follow cluster_follower init http://46.101.179.114:8080/ipfs/QmRNJD4dXVfaqSfskY8LT2mdxbELiHVAkqPuWdz9WgQfxS
-ipfs-cluster-follow cluster_follower run
-# curl -X POST "http://103.73.101.34:5001/api/v0/bootstrap/add?arg=/ip4/46.101.179.114/tcp/4001/p2p/12D3KooWQcT1db4e1XiWc2HqvCwrMeTANr3QeQquynJ3WHTB4ucV"
+ipfs-cluster-follow cluster_follower run zain.haider@invozone.com:Asdfghjkl1!:649be63b7db205ac14e197ab
