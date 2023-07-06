84f216b7de94cf7f6e9f63a3474208624268234d
Sheraz Ahmed <sheraz@invozone.com>
2023-07-06 13:21:02 +0500

    WIP: Update Configs to Reflect Node 1 as master Node
    - Updated Swarmkey
    - Updated Bootstrap URL
    - Updated Configuration and published them to ipns static url

--- follower.sh
+++ follower.sh
@@ -1,2 +1,4 @@
-ipfs-cluster-follow cluster_follower init http://46.101.179.114:8080/ipfs/QmRNJD4dXVfaqSfskY8LT2mdxbELiHVAkqPuWdz9WgQfxS
+# TODO: Throughly Test the configurations
+# ipfs-cluster-follow cluster_follower init http://46.101.179.114:8080/ipfs/QmRNJD4dXVfaqSfskY8LT2mdxbELiHVAkqPuWdz9WgQfxS
+ipfs-cluster-follow cluster_follower init http://46.101.133.110:8080/ipns/k51qzi5uqu5dlh2h4gml3k5vik5mas2zos0pyf1shnfzj9d4nsg31lw4x7ujsk
 ipfs-cluster-follow cluster_follower run $CLUSTER_EMAIL:$CLUSTER_PASSWORD:$CLUSTER_NODE_ID
